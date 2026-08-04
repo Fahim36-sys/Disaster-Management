5fa40cd804d8bd20f560e0acbebc7fc48cf398bc
Fahim36-sys <fahimhasnat32@outlook.com>
2026-08-04 12:29:22 +0600

    Co-authored-by: shaodashreya678-lab <shaodashreya678-lab@users.noreply.github.com>

---
+++ Citizen.java
@@ -0,0 +1,23 @@
+public class Citizen{
+    private Person person;
+    private String citizenID;
+    private String address;
+    private String reportType;
+    public Citizen(){}
+    public Citizen(String citizenID,String address,String reportType){
+        this.citizenID = citizenID;
+        this.address = address;
+        this.reportType = reportType;
+    }
+    public void setPerson(Person person){this.person = person;}
+    public Person getPerson(){return person;}
+    public String getCitizenID(){return citizenID;}
+    public void setCitizenID(String citizenID) {
+        this.citizenID = citizenID;
+    }
+    public String getAddress(){ return address;}
+    public void setAddress(String address){this.address = address;}
+    public String getReportType() { return reportType;}
+
+    public void setReportType(String reportType) { this.reportType = reportType;}
+}
