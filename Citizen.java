04c3a6ba70aa6fbb2f0119c493c9298f34ae288b
Fahim36-sys <fahimhasnat32@outlook.com>
2026-08-04 21:45:26 +0600

    ..

--- Citizen.java
+++ Citizen.java
@@ -5,19 +5,18 @@ public class Citizen{
     private String reportType;
     public Citizen(){}
     public Citizen(String citizenID,String address,String reportType){
-        this.citizenID = citizenID;
-        this.address = address;
-        this.reportType = reportType;
+        this.citizenID=citizenID;
+        this.address=address;
+        this.reportType=reportType;
     }
-    public void setPerson(Person person){this.person = person;}
+    public void setPerson(Person person){this.person=person;}
     public Person getPerson(){return person;}
     public String getCitizenID(){return citizenID;}
-    public void setCitizenID(String citizenID) {
-        this.citizenID = citizenID;
+    public void setCitizenID(String citizenID){
+        this.citizenID=citizenID;
     }
-    public String getAddress(){ return address;}
-    public void setAddress(String address){this.address = address;}
-    public String getReportType() { return reportType;}
-
-    public void setReportType(String reportType) { this.reportType = reportType;}
+    public String getAddress(){return address;}
+    public void setAddress(String address){this.address=address;}
+    public String getReportType(){return reportType;}
+    public void setReportType(String reportType){this.reportType = reportType;}
 }
