6839a770f23314fab9be8ceaab96cb3efe1f04c9
Fahim36-sys <fahimhasnat32@outlook.com>
2026-08-19 14:44:12 +0600

    Co-authored-by: Shaoda <shaodashreya678-lab@users.noreply.github.com>

--- ResponseTeam.java
+++ ResponseTeam.java
@@ -2,19 +2,23 @@ public class ResponseTeam{
     private String teamid;
     private String teamname;
     private String numberOfteammembers;
+    private Responder[] responders;
     public ResponseTeam(){}
-    public ResponseTeam(String teamid, String teamname, String numberOfteammembers){
+    public ResponseTeam(String teamid, String teamname, String numberOfteammembers, Responder[] responders){
         this.teamid=teamid;
         this.teamname=teamname;
         this.numberOfteammembers=numberOfteammembers;
+        this.responders=responders;
     }
     public void setteamid(String teamid){this.teamid=teamid;}
     public void setteamname(String teamname){this.teamname=teamname;}
     public void setnumberOfTeammembers(String numberOfteammembers){this.numberOfteammembers=numberOfteammembers;}
+    public void setResponders(Responder[] responders){this.responders=responders;}
 
     public String getteamid(){return teamid;}
     public String getteamname(){return teamname;}
     public String getnumberOfTeamMembers(){return numberOfteammembers;}
+    public Responder[] getResponders(){return responders;}
 
     public void showDetails(){
         System.out.println("Team ID:" +teamid);
